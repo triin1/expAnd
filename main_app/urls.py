a78e0fc6d0242136000002b19f2e99a2a5f217fc
triin <triin.sikk@gmail.com>
2024-04-13 22:07:20 +1000

    create expense mock-up

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+
+    # path('expenses/create', views.ExpenseCreate.as_views, name='expense_create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+# from .models import Expense
 
 
 def home(request):
     return render(request, 'home.html')
+
+# class ExpenseCreate(CreateView):
+#     model = Expense
+#     fields = '__all__'
