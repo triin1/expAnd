fd1cee5a91884f385bd3f6b7880ab724b95ab670
triin <triin.sikk@gmail.com>
2024-04-22 23:51:19 +1000

    Minor formatting updates

--- expAnd/settings.py
+++ expAnd/settings.py
@@ -120,6 +120,7 @@ STATIC_URL = 'static/'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+USE_THOUSAND_SEPARATOR = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
